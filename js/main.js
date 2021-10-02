bd7bbd393a8a7f83f6aba794bb3735e058d11ecb
DariaAkimova <akimova-dv@mail.ru>
2021-10-02 19:23:47 +0300

    После доработки

--- js/main.js
+++ js/main.js
@@ -1,36 +1,34 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable prefer-template */
 /* eslint-disable no-console */
 function getRandomInt(from, to) {
-  if (from < 0) {
+  if (from < 0 || to <0) {
     return;
   }
-  if (from < to) {
-    from = Math.ceil(from);
-    to = Math.floor(to);
-    console.log (Math.floor(Math.random() * (to - from + 1)) + from);
+  if (from === to) {
+    console.log ('Единственное возможное значение: ' + from);
   } else if (from >to ) {
     [from, to]=[to,from];
-    from = Math.ceil(from);
-    to = Math.floor(to);
-    console.log (Math.floor(Math.random() * (to - from + 1)) + from);
-  } else {
-    console.log ('Единственное возможное значение: ' + from);
   }
+  from = Math.ceil(from);
+  to = Math.floor(to);
+  console.log (Math.floor(Math.random() * (to - from + 1)) + from);
 }
 getRandomInt();
 
 
-function getRandomNumber(from, to) {
-  if (from < 0){
+function getRandomNumber(from, to, digits) {
+  if (from < 0 || to <0){
     return;
   }
-  if (from < to) {
-    console.log (Math.random() * (to - from) + from);
+  if (from === to) {
+    console.log ('Единственное возможное значение: ' + from);
   } else if (from >to ) {
     [from, to]=[to,from];
-    return (Math.random() * (to - from) + from);
-  } else {
-    console.log ('Единственное возможное значение: ' + from);
   }
+  const randomNumber = Math.random() * (to - from) + from;
+  console.log (+randomNumber.toFixed(digits));
+
 }
 getRandomNumber();
+
